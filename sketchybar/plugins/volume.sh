140092f8f60576dcee87f56060e670a1184e733f
brian <bw7520@princeton.edu>
2026-05-21 03:40:10 -0400

    Fix sketchybar right-side layout

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -7,20 +7,22 @@ source "$CONFIG_DIR/colors.sh"
 
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
-
-  case "$VOLUME" in
-    [6-9][0-9]|100) ICON=""
-    ;;
-    [3-5][0-9]) ICON=""
-    ;;
-    [1-9]|[1-2][0-9]) ICON=""
-    ;;
-    *) ICON=""
-  esac
-
-  sketchybar --animate tanh 10 --set "$NAME" \
-    icon="$ICON" \
-    icon.color="$COLOR_IRIS" \
-    label="$VOLUME%" \
-    label.color="$COLOR_IRIS"
+else
+  VOLUME="$(osascript -e 'output volume of (get volume settings)')"
 fi
+
+case "$VOLUME" in
+  [6-9][0-9]|100) ICON=""
+  ;;
+  [3-5][0-9]) ICON=""
+  ;;
+  [1-9]|[1-2][0-9]) ICON=""
+  ;;
+  *) ICON=""
+esac
+
+sketchybar --animate tanh 10 --set "$NAME" \
+  icon="$ICON" \
+  icon.color="$COLOR_IRIS" \
+  label="$VOLUME%" \
+  label.color="$COLOR_IRIS"
